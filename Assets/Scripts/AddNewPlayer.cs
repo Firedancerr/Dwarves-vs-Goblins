6b970251e31261f70a0fffb6f76fcfa194b44648
Firedancerr <milosz6e3@wp.pl>
2019-04-08 15:17:33 +0200

    V 0.3

---
+++ Assets/Scripts/AddNewPlayer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AddNewPlayer : MonoBehaviour
+{
+    public GameObject PopUp;
+    public Text PlayerList;
+    public Text PlayerName;
+
+    private void Start()
+    {
+        PopUp.SetActive(false);
+    }
+    public void Clicked(int popupHide)
+    {
+        if (popupHide == 0)
+        {
+            PopUp.SetActive(true);
+        }
+        if (popupHide == 1)
+        {
+            PopUp.SetActive(false);
+            GameScript.PlayerCount++;
+            PlayerList.text = PlayerList.text + "\n" + PlayerName.text + " " + GameScript.PlayerCount.ToString();
+            GameScript.Players.Add(PlayerName.text);
+            PlayerName.text = "";
+        }
+    }
+}
---
+++ Assets/Scripts/GameScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameScript : MonoBehaviour
+{
+    public static int PlayerCount;
+    public static int PlayerCountAlive;
+    public static List<string> Players;
+    public static string[] DoorsFinal;
+    public static string[] PickColors = { "red", "green", "brown", "black", "white" };
+    public static string[] DoorExtra = {"none", "visor","door number"};
+    public static string[] PickMaterials = {"wooden","metal"};
+    public static string[] DoorMat = {"square","triangle","circle" };
+    public static string[] DoorKnob = { "circle", "straight" };
+    public static string[] DoorFinish = {"none"};
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
