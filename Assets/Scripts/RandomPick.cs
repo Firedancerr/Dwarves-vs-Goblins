06580105444275f43da6050aee2647c0a1421ef0
Firedancerr <milosz6e3@wp.pl>
2019-05-21 15:49:40 +0200

    V 0.6.1

--- Assets/Scripts/RandomPick.cs
+++ Assets/Scripts/RandomPick.cs
@@ -21,17 +21,25 @@ public class RandomPick : MonoBehaviour
         
     }
 
-    public void Random2Doors()
+    public void RandomDoors(int j)
     {
+        string[] Colors = new string[GameScript.PickColors.Length];
+        string[] Extra = new string[GameScript.DoorExtra.Length];
+        string[] Materials = new string[GameScript.PickMaterials.Length];
+        string[] Mat = new string[GameScript.DoorMat.Length];
+        string[] Knob = new string[GameScript.DoorKnob.Length];
+        string[] Finish = new string[GameScript.DoorFinish.Length];
         SameType1 = Random.Range(0, 7);
-        for (int i=1;i<=2;i++)
+        SameType2 = Random.Range(0, 7);
+        SameType3 = Random.Range(0, 7);
+        for (int i=1; i<=j;i++)
         { 
-        RandomedColor = Random.Range(0, 6);
-        RandomedExtra = Random.Range(0, 5);
-        RandomedMaterial = Random.Range(0, 5);
-        RandomedMat = Random.Range(0, 5);
-        RandomedKnob = Random.Range(0, 5);
-        RandomedFinish = Random.Range(0, 5);
+        RandomedColor = Random.Range(0, Colors.Length);
+        RandomedExtra = Random.Range(0, Extra.Length);
+        RandomedMaterial = Random.Range(0, Materials.Length);
+        RandomedMat = Random.Range(0, Mat.Length);
+        RandomedKnob = Random.Range(0, Mat.Length);
+        RandomedFinish = Random.Range(0, Finish.Length);
         string[] RandomDoors = {RandomedColor.ToString(),RandomedExtra.ToString(),RandomedMaterial.ToString(), RandomedMat.ToString(), RandomedKnob.ToString(), RandomedFinish.ToString() };
             if (i==1)
             {
@@ -42,10 +50,20 @@ public class RandomPick : MonoBehaviour
                 GameScript.DoorsFinalBad = RandomDoors;
                 GameScript.DoorsFinalBad[SameType1] = GameScript.DoorsFinalGood[SameType1];
             }
-            
+            if (i == 3)
+            {
+                GameScript.DoorsFinalBad = RandomDoors;
+                GameScript.DoorsFinalBad[SameType2] = GameScript.DoorsFinalGood[SameType2];
+            }
+            if (i == 4)
+            {
+                GameScript.DoorsFinalBad = RandomDoors;
+                GameScript.DoorsFinalBad[SameType3] = GameScript.DoorsFinalGood[SameType3];
+            }
+
         }
     }
-
+    /*
     public void Random3Doors()
     {
         SameType1 = Random.Range(0, 7);
@@ -111,7 +129,7 @@ public class RandomPick : MonoBehaviour
             }
 
         }
-    }
+    }*/
 
 
 
